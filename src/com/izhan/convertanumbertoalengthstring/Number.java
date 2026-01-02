0adbe99973fd2b682d74b8a52d49679bb23f87e8
izhank216 <izhank216@gmail.com>
2026-01-02 17:15:40 +0500

    Create Number.java

---
+++ src/com/izhan/convertanumbertoalengthstring/Number.java
@@ -0,0 +1,13 @@
+package com.izhan.convertanumbertoalengthstring;
+
+public class Number {
+    private double value;
+
+    public Number(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
